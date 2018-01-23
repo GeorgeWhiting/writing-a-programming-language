c95deafd20a8e8c1e317dd5cfed8bfd219288b20
Lars <larsfinlay@googlemail.com>
2018-01-23 14:21:04 +0000

    basic Testing framework setup

---
+++ spec/Assertion.java
@@ -0,0 +1,9 @@
+public class Assertion {
+
+    public String assertTest(String test, Integer expect) {
+        Interpreter testinterpreter = new Interpreter(test);
+        Integer result = testinterpreter.expr();
+        return (result == expect ? "TEST PASSED" : "TEST FAILED: expected " + Integer.toString(expect) + " got " + Integer.toString(result));
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
 
-<<<<<<< HEAD
-        Interpreter interpreter = new Interpreter("2+2");
-        Integer result = interpreter.expr();
-        System.out.println(result);
-=======
         String input;
         Scanner reader = new Scanner(System.in);
         System.out.println("Type in stop to end process");
@@ -22,7 +17,6 @@ public class Main {
             Integer result = interpreter.expr();
             System.out.println(result);
         }
->>>>>>> ca98032a4fae4d8ae9f9b6f391a32e632aedf942
 
     }
 }
