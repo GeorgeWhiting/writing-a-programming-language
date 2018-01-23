7b0a5b3753d711f9b63448cfdc5272f457c72b63
Lars <larsfinlay@googlemail.com>
2018-01-23 16:16:00 +0000

    configuring object alteration into string

--- spec/Assertion.java
+++ spec/Assertion.java
@@ -2,8 +2,8 @@ public class Assertion {
 
     public static String assertTest(String test, Integer expect) {
         Interpreter testinterpreter = new Interpreter(test);
-        Integer result = testinterpreter.expr();
-        return (result.equals(expect) ? "TEST PASSED" : "TEST FAILED: expected " + Integer.toString(expect) + " got " + Integer.toString(result));
+        Object result = testinterpreter.expr();
+        return (result.equals(expect) ? "TEST PASSED" : "TEST FAILED: expected " + Integer.toString(expect) + " got " + String.valueOf(result));
     }
 
 }
