6dea81e83b116774ebe376938ef2ba88ed929360
George <george_whiting@hotmail.com>
2018-01-23 13:55:23 +0000

    fixes merge

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-
-<<<<<<< HEAD
-        Interpreter interpreter = new Interpreter("2+2");
-        Integer result = interpreter.expr();
-        System.out.println(result);
-=======
         String input;
         Scanner reader = new Scanner(System.in);
         System.out.println("Type in stop to end process");
@@ -22,7 +16,6 @@ public class Main {
             Integer result = interpreter.expr();
             System.out.println(result);
         }
->>>>>>> ca98032a4fae4d8ae9f9b6f391a32e632aedf942
 
     }
 }
