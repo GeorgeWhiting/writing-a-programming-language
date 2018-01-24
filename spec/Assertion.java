710d14f37040ff1953db18f4b8f48b87819e5b65
tom <tomallpress@hotmail.com>
2018-01-24 17:03:22 +0000

    tests can not be grouped and ran in groups

--- spec/Assertion.java
+++ spec/Assertion.java
@@ -19,7 +19,6 @@ public class Assertion {
             return "Error";
 
         }
+
     }
-
 }
-
--- spec/TestRunner.java
+++ spec/TestRunner.java
@@ -12,26 +12,28 @@ public class TestRunner {
     }
 
     public static void main(String[] args) {
-        ArrayList<Test> testArray = new ArrayList<Test>();
+        ArrayList<Test> testBasicMath = new ArrayList<Test>();
+        ArrayList<Test> testToken = new ArrayList<Test>();
         Assertion asserter = new Assertion();
-        new Test("1+1", 2, testArray);
-        new Test("3 + 3", 6, testArray);
-        new Test("333 + 333", 666, testArray);
-        new Test("1    +   1", 2, testArray);
-        new Test("1111 + 1111", 2222, testArray);
-        new Test("3 - 1", 2, testArray);
-        new Test("3*3", 9, testArray);
-        new Test("    6 * 12", 72, testArray);
-        new Test("1 + 2 + 3", 6, testArray);
-        new Test("1 + 5", "1", testArray);
-        new Test("713 + 1230", "713", testArray);
-        new Test("1 + 2 * 2", 5, testArray);
-        new Test("3/3", 1, testArray);
-        new Test("1+(3*3)", 10, testArray);
-        new Test("1+((3*3)+4)", 14, testArray);
-        new Test("1+((3/3)*4)", 5, testArray);
-        new Test("(1+((3/3)*4))+3", 8, testArray);
-        runTests(testArray);
+        new Test("1+1", 2, testBasicMath);
+        new Test("3 + 3", 6, testBasicMath);
+        new Test("333 + 333", 666, testBasicMath);
+        new Test("1    +   1", 2, testBasicMath);
+        new Test("1111 + 1111", 2222, testBasicMath);
+        new Test("3 - 1", 2, testBasicMath);
+        new Test("3*3", 9, testBasicMath);
+        new Test("    6 * 12", 72, testBasicMath);
+        new Test("1 + 2 + 3", 6, testBasicMath);
+        new Test("1 + 2 * 2", 5, testBasicMath);
+        new Test("3/3", 1, testBasicMath);
+        new Test("1+(3*3)", 10, testBasicMath);
+        new Test("1+((3*3)+4)", 14, testBasicMath);
+        new Test("1+((3/3)*4)", 5, testBasicMath);
+        new Test("(1+((3/3)*4))+3", 8, testBasicMath);
+        runTests(testBasicMath);
+        new Test("1 + 5", "1", testToken);
+        new Test("713 + 1230", "713", testToken);
+        runTests(testToken);
     }
 
 }
