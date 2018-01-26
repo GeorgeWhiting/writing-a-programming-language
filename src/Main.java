bbce0821db90a3639e046cf91a14f14959f988f2
Lars <larsfinlay@googlemail.com>
2018-01-26 13:59:08 +0000

    Repl improvement

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ public class Main {
     public static void main(String args[]){
         String longInput = "";
         String input;
+        String replInput = "MOM >> ";
         Scanner reader = new Scanner(System.in);
         System.out.println("Type in stop or quit to end process");
+        int skullbone = 9760;
+        String s = Character.toString((char) skullbone);
 
         while(true) {
-            System.out.print("lavajava > ");
+            System.out.print(replInput);
             input = reader.nextLine();
             if(input.equals("stop")){
-                System.out.println("lavajava > " + "goodbye");
+                System.out.println(replInput + "goodbye");
                 break;
             }
             if(input.equals("quit")){
-                System.out.println("lavajava > " + "goodbye");
+                System.out.println(replInput + "goodbye");
                 break;
             }
 
@@ -23,19 +26,27 @@ public class Main {
             if(input.equals("please")){
                 longInput = "please  ";
                 while(!input.equals("thanks.")){
-                    System.out.print("lavajava > ");
+                    System.out.print(replInput);
                     input = reader.nextLine();
                     longInput += input;
                 }
                 Lexer lexer = new Lexer(longInput);
                 Parser parser = new Parser(lexer);
                 Interpreter interpreter = new Interpreter(parser);
-                Object result = interpreter.interpret();
+                try {
+                    Object result = interpreter.interpret();
+                } catch(Exception e) {
+                    System.out.println(s + " " + e);
+                }
             }  else {
                 Lexer lexer = new Lexer(input);
                 Parser parser = new Parser(lexer);
                 Interpreter interpreter = new Interpreter(parser);
-                Object result = interpreter.interpret();
+                try {
+                    interpreter.interpret();
+                } catch(Exception e) {
+                    System.out.println(s + " " + e);
+                }
             }
         }
     }
