dd9b21548c959baf35cdf25aca29a52f8328c4ff
tom <tomallpress@hotmail.com>
2018-01-26 12:34:07 +0000

    able to input code on multiple lines

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
+        String longInput = "";
         String input;
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type in stop to end process");
+        System.out.println("Type in stop or quit to end process");
 
         while(true) {
+            System.out.print("lavajava > ");
             input = reader.nextLine();
             if(input.equals("stop")){
-                System.out.println("Stopping....");
+                System.out.println("lavajava > " + "goodbye");
                 break;
             }
-            Lexer lexer = new Lexer(input);
-            Parser parser = new Parser(lexer);
-            Interpreter interpreter = new Interpreter(parser);
-            Object result = interpreter.interpret();
-           // System.out.println(result);
+            if(input.equals("quit")){
+                System.out.println("lavajava > " + "goodbye");
+                break;
+            }
+
+
+            if(input.equals("please")){
+                longInput = "please  ";
+                while(!input.equals("thanks.")){
+                    System.out.print("lavajava > ");
+                    input = reader.nextLine();
+                    longInput += input;
+                }
+                Lexer lexer = new Lexer(longInput);
+                Parser parser = new Parser(lexer);
+                Interpreter interpreter = new Interpreter(parser);
+                Object result = interpreter.interpret();
+            }  else {
+                Lexer lexer = new Lexer(input);
+                Parser parser = new Parser(lexer);
+                Interpreter interpreter = new Interpreter(parser);
+                Object result = interpreter.interpret();
+            }
         }
     }
 }
+
+
